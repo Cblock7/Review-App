9f60f5bd7f98b8610465500cbe8ae9475418bff8
Zach Raab <zpraab@hotmail.com>
2021-06-01 14:39:06 -0600

    CRUD operations for reviews

--- controllers/api/review-routes.js
+++ controllers/api/review-routes.js
@@ -1,13 +1,63 @@
 const router = require("express").Router();
 const Reviews = require("../../models/Reviews");
 
+// Get all reviews
 router.get("/", async (req, res) => {
-  console.log("hello");
   try {
     const findAllReviews = await Reviews.findAll();
     res.status(200).json(findAllReviews);
   } catch (err) {
-    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Get a review by it's user_id
+router.get("/:user_id", async (req, res) => {
+  try {
+    const findOneReview = await Reviews.findByPk(req.params.user_id);
+
+    if (!findOneReview) {
+      res.status(404).json({ message: "Unable to find review with that ID" });
+    }
+    res.status(200).json(findOneReview);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Create a new review
+router.post("/", async (req, res) => {
+  try {
+    const createdReview = await Reviews.create(req.body);
+    res.status(200).json(createdReview);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Update a review by it's user_id
+router.put("/:user_id", async (req, res) => {
+  try {
+    const updatedReview = await Reviews.update(req.body, {
+      where: { user_id: req.params.user_id },
+    });
+    res.status(200).json(updatedReview);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Delete a review by it's user_id
+router.delete("/:user_id", async (req, res) => {
+  try {
+    const deletedReview = await Reviews.destroy({
+      where: { user_id: req.params.user_id },
+    });
+    if (!deletedReview) {
+      res.status(404).json({ message: "Unable to find post with that ID" });
+    }
+    res.status(200).json(deletedReview);
+  } catch (err) {
     res.status(500).json(err);
   }
 });
