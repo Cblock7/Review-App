174334b3df48d9504b11afadee786b4a3bdde658
Joshua Abeyta <mentaltemple@gmail.com>
2021-06-02 14:06:12 -0600

    fixed server.js by adding app.use static pathjoin public

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const Sequelize = require("./config/connection");
 const express = require("express");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
+const path = require("path")
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +13,7 @@ app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")))
 app.use(routes);
 
 Sequelize.sync({ force: false }).then(() => {
