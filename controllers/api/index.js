f7c832a4494fb4dd3314b3bafd0932258c021967
Zach Raab <zpraab@hotmail.com>
2021-06-02 13:56:08 -0600

    concert route handlers, concerts and review table joining

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const reviewRoutes = require("./review-routes");
+const concertsRoutes = require("./concerts-routes");
 
 router.use("/reviews", reviewRoutes);
+router.use("/concerts", concertsRoutes);
 
 module.exports = router;
