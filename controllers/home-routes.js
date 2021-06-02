c55bf53f1dcda70731c5ad45589404768eb376a6
Joshua Abeyta <mentaltemple@gmail.com>
2021-06-02 12:11:43 -0600

    finished linking handlebars in server.js, began building out handlebar files in dir views

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,12 +1,17 @@
 const router = require("express").Router();
-const fs = require("fs")
-const path = require("path")
+const fs = require("fs");
+const path = require("path");
+const Reviews = require("../models/Reviews");
+
 // const sequelize = require('../config/connection');
 
 router.get("/", async (req, res) => {
   // console.log("test");
   try {
-    res.sendFile(path.join(__dirname, '../views/index.html'));
+    const moshData = await Reviews.findAll();
+
+    const mosh = moshData.map((reviews) => reviews.get({ plain: true }));
+    res.render("homepage", mosh);
   } catch (err) {
     res.status(500).json(err);
   }
