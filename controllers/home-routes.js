6b60a96c9c818b809e100d4977cb9e60eaf1073e
Corey Block <corey.block7@gmail.com>
2021-06-03 13:03:58 -0600

    added script tag in homepage.handlebars, commented out the concert_id

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,17 +7,15 @@ const Reviews = require("../models/Reviews");
 
 router.get("/", async (req, res) => {
   try {
+    
     const moshData = await Reviews.findAll();
 
     const mosh = moshData.map((reviews) => reviews.get({ plain: true }));
     res.render("homepage");
   } catch (err) {
     res.status(500).json(err);
+
   }
 });
 
-<<<<<<< HEAD
 module.exports = router;
-=======
-module.exports = router;
->>>>>>> 14ae232ad659f95e99b71291d230359a8bfd2132
